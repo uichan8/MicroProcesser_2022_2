e847c18569c8a6fdcd9c3c349e854c1c1117af74
uichan <uichan980202@gmail.com>
2022-11-08 12:53:37 +0900

    add exp 010

---
+++ EXP010_LCD/EXP010_LCD.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal.h>
+int rs = 13, en = 12, d4 = 14, d5 = 27, d6 = 26, d7 = 25;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(20, 4);
+}
+
+int count = 0;
+void loop() {
+  int star = 2;
+
+  lcd.setCursor((20-star)/2, 1);
+  for(int i = 0; i < star; i++){
+    if(count%2)lcd.write('*');
+    else lcd.write(' ');
+  }
+    
+  lcd.setCursor((20-star)/2, 2);
+  for(int i = 0; i < star; i++){
+    if(count%2)lcd.write(' ');
+    else lcd.write('*');
+  }
+
+  delay(1000);
+  count++;
+}
